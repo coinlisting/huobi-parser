7d0493dd843ddd108ee3ecf2449b8e56a93d85f5
Erbolbaysalov <49031726+Erbolbaysalov@users.noreply.github.com>
2021-11-21 02:25:14 +0600

    add file

--- database/store_file.py
+++ database/store_file.py
@@ -1,12 +1,17 @@
 import json
+import os
 from json.decoder import JSONDecodeError
 
 def writing_file(file, data):
     with open(file, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False,indent=2)
 def reading_file(file):
-    with open(file, "r", encoding='utf-8') as f:
-        try: 
-            return json.load(f)
+    if not os.path.exists(file):
+        with open(file, 'w'):
+            pass
+    with open(file, "r") as f:
+        try:
+            data = json.load(f)
+            return data
         except JSONDecodeError:
-            return {"TEXT":''}
+            return None
